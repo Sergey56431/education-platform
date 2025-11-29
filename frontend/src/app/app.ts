df918150f30c8462f7a82833c6d62d6b6f1ff890
sergey56431 <sergey56431@gmail.com>
2025-11-29 14:29:00 +0600

    chore: проиндексировал файлы

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Header} from '@core/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'frontend';
+
 }
